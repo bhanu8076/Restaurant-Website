8cc518266939fb9003a7d5299e878be125b805d3
bhanu8076 <bs0524676@gmail.com>
2020-05-16 12:11:08 +0530

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
- 
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent'
 
 class Menu extends Component {
 
@@ -40,8 +38,8 @@ class Menu extends Component {
             return (
               <div  className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        onClick={() => this.props.onClick(dish.id)}>
+                  <CardImg top src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
                   </CardImgOverlay>
@@ -55,11 +53,10 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                
+                
+        
+        
             </div>
         );
     }
