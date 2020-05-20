fc86f839e13f5e311a06123cf1bb50f4ce9c4406
bhanu8076 <bs0524676@gmail.com>
2020-05-20 13:59:00 +0530

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import './App.css';
 
+const store = ConfigureStore();
+
 class App extends Component {
 
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
       <div className="App">
         <Main />
       </div>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
