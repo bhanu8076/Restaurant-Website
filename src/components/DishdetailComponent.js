a313521ba0a8c71d2a08f218aa54f2e1f41c23ec
bhanu8076 <bs0524676@gmail.com>
2020-05-21 21:54:24 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 
 
-    function RenderComments({comments}) {
+    function RenderComments({comments,addComment, dishId}) {
         if (comments == null) {
             return (<div></div>);
         }
@@ -32,7 +32,7 @@ import { Link } from 'react-router-dom';
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
     }
@@ -77,7 +77,10 @@ import { Link } from 'react-router-dom';
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
                     </div>
                 </div>
                 </div>
@@ -119,7 +122,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
     handleSubmit(values) {
         this.toggleModal();
 
-        alert(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
     render() {
         return (
